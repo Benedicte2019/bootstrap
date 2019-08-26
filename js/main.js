56803ba4e97c091e6b1c6d77aa01cad89984005a
Alpha Olomi <alphaolomi@gmail.com>
2019-08-26 09:54:01 +0300

    re organise

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+  //lines below were added by mery
+  //$("#formButton").click(function() {
+    //$("#form1").toggle();
+  //});
+})
+
+
+
